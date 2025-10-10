3e8dd059e18860848ae8f94bb3ca13e44990e531
harsh <nh1750501@gmail.com>
2025-10-10 22:46:15 +0530

    Updated code base

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main(){
   vector<int> v={2,3,45,6};
   v.push_back(45);
+
+  vector<int>::iterator itr;
+  cout<<"using iterator";
+  for (itr = v.begin();itr!=v.end();itr++){
+    cout<<itr<<endl;
+  }
   for(int x:v){
     cout<<x<<endl;
   }
